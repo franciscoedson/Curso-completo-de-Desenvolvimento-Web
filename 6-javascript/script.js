b743cebb3c37dbc0a72da7b9c7881f20bac72f63
Francisco Edson <fedrosansonicc@gmail.com>
2020-04-19 20:26:37 -0300

    js add 2

---
+++ 6-javascript/script.js
@@ -0,0 +1,21 @@
+// Aula 1
+// alert("jasjdsaksdfj j sjs kdjfkj jsdf sjkj")
+
+
+// Aula 2
+// string
+var texto = "Aula ";
+var texto2 = prompt("Digite seu nome");
+
+// float
+var nFracionado = 10.7;
+
+// int
+var n = -7;
+
+//bool
+var teste = true;
+
+// alert(texto);
+document.write(texto + texto2);
+console.log(teste);
